054b838c83fa09b5d8cac6449e0841b814560fe5
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-09 12:24:44 -0400

    router end points set up

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,6 @@
 // Write your "actions" router here!
 const express = require('express')
+const { route } = require('../projects/projects-router')
 const Actions = require('./actions-model')
 
 const router = express.Router()
@@ -8,6 +9,35 @@ const router = express.Router()
 router.get('/', (req,res) => {
   res.send('actions get request')
 })
+
+// [GET] /api/actions/:id
+router.get('/:id', (req,res) => {
+  res.send('action get with id')
+})
+
+// [POST] /api/actions
+router.post('/', (req,res) => {
+  res.send('actions post request')
+})
+
+// [PUT] /api/actions/:id
+router.put('/:id', (req,res) => {
+  res.send('actions put request')
+})
+
+//[DELETE] /api/actions/:id
+router.delete('/id', (req,res) => {
+  res.send('actions delete request')
+})
+
+
+router.use((err,req,res,next) => {
+  res.status(500).json({
+    message: 'Something broken',
+    error: err.message
+  })
+})
+
 
 
 module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,37 @@
 // Write your "projects" router here!
 const express = require('express')
+const Projects = require('./projects-model')
 
 const router = express.Router()
 
 // [GET] /api/projects
 router.get('/', (req,res) => {
-  res.send('get request')
+  res.send('get request projects')
+})
+
+// [GET] /api/projects/:id
+router.get('/:id', (req,res) => {
+  res.send('get project with id')
+})
+
+// [POST] /api/projects
+router.get('/', (req,res) => {
+  res.send('post request in project')
+})
+
+// [PUT] /api/projects/:id
+router.put('/:id', (req,res) => {
+  res.send('put request in projects')
+})
+
+// [DELETE] /api/projects/:id
+router.delete('/:id', (req,res) => {
+  res.send('delete request in projects')
+})
+
+// [GET] /api/projects/:id/actions
+router.get('/:id/actions', (req,res) => {
+  res.send('reciving a list of actoins from project')
 })
 
 module.exports = router
